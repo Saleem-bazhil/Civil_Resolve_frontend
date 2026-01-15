c5ab26bda6d00b3bc771ab6a93e85e23d082320f
saleem <ab588538@gmail.com>
2026-01-15 13:04:03 +0530

    first commit

--- components/ScreenContent.tsx
+++ components/ScreenContent.tsx
@@ -16,6 +16,7 @@ export const ScreenContent = ({ title, path, children }: ScreenContentProps) => 
       <View className={styles.separator} />
       <EditScreenInfo path={path} />
       {children}
+      <Text>Civil Resolve</Text>
     </View>
   );
 };
