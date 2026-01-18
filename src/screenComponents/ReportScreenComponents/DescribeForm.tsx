a6cba013c4814685e2fc7081677854ddc0379eb3
saleem <ab588538@gmail.com>
2026-01-18 13:07:16 +0530

    add the describeform

---
+++ src/screenComponents/ReportScreenComponents/DescribeForm.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const DescribeForm = () => {
+  return (
+    <View>
+      <Text>DescribeForm</Text>
+    </View>
+  )
+}
+
+export default DescribeForm
